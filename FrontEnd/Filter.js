3a3fb67eb94d7ab6761b5f648375e0b957894440
Cinho <marciolacourcelle@gmail.com>
2025-10-13 12:24:03 +0200

    refonte du style de la page + ajout de la page login

--- FrontEnd/Filter.js
+++ FrontEnd/Filter.js
@@ -1,50 +1,40 @@
 import { showAllWorks } from "./Function.js";
+import { fetchWorks } from "./Function.js";
+import {fetchCategories} from "./Function.js";
+import { displayWorks } from "./Function.js";
+import { activeStyleButton } from "./Function.js";
 
-showAllWorks();
-
-fetch ('http://localhost:5678/api/categories')
-.then(reponse => reponse.json())
-.then (categories => {
+showAllWorks();//affiche tout par default
+//creation du bouton "tout" avec la fonction showAllWork en effet
+fetchCategories().then (categories => {
 const filtersContainer = document.querySelector(".filters");
 const buttonFiltersReset = document.createElement("button");
 buttonFiltersReset.textContent = "Tous";
+buttonFiltersReset.id = "idButton-0";
+buttonFiltersReset.classList.add('filters-button');
 filtersContainer.appendChild(buttonFiltersReset);
 buttonFiltersReset.addEventListener("click", () => {
+activeStyleButton(buttonFiltersReset, "filters-button", "filters-active")
   showAllWorks();
 });
 
-
-categories.forEach (categories =>{
-const buttonFilter = document.createElement("button");
-buttonFilter.textContent = categories.name;
-buttonFilter.id = "idButton-" + categories.id
-filtersContainer.appendChild(buttonFilter);
-
-
-fetch("http://localhost:5678/api/works")
-    .then(response => response.json())
-    .then(works => {
-buttonFilter.addEventListener("click", () => {
-  document.querySelector(".gallery").innerHTML = '';
-  const resultat = works.filter(works => works.category.name === categories.name);
-  resultat.forEach(works => {
-    const figure = document.createElement("figure");
-        figure.innerHTML = `
-      <img src="${works.imageUrl}" alt="${works.title}" />
-      <figcaption>${works.title}</figcaption>
-    `;
-    document.querySelector(".gallery").appendChild(figure);
+//création de chaque boutton par nombre de categories
+categories.forEach(categories =>{
+  const buttonFilter = document.createElement("button");
+  buttonFilter.textContent = categories.name;
+  buttonFilter.id = "idButton-" + categories.id;
+  buttonFilter.classList.add('filters-button');
+  filtersContainer.appendChild(buttonFilter);
+  //filtre qui compare le nom de la categorie(donc du bouton)
+  //  et le nom du travail, si le resultat est true = afficher
+  buttonFilter.addEventListener("click", () => {
+    //ajout et retrait des classe selon le click
+    activeStyleButton(buttonFilter,"filters-button", "filters-active")
+    document.querySelector(".gallery").innerHTML = '';
+    fetchWorks().then(works => {
+      const resultat = works.filter(works => works.category.name === categories.name);
+      displayWorks(resultat, document.querySelector(".gallery"));
+    });
   });
-
 });
-});
-
-
-});
-
-
-
-
-
-
 });
--- FrontEnd/Function.js
+++ FrontEnd/Function.js
@@ -1,18 +1,35 @@
+export function fetchWorks() {
+  return fetch("http://localhost:5678/api/works")
+    .then(response => response.json());
+};
 
-export function showAllWorks() {
+export function fetchCategories() {
+  return fetch('http://localhost:5678/api/categories')
+    .then(response => response.json());
+};
 
-    fetch("http://localhost:5678/api/works")
-    .then(response => response.json())
-    .then(works => {
-        document.querySelector(".gallery").innerHTML = '';
-        works.forEach(work => {
+export function displayWorks (worksToDisplay, destination) {
+     worksToDisplay.forEach(work => {
             const figure = document.createElement("figure");
             figure.innerHTML = `
             <img src="${work.imageUrl}" alt="${work.title}" />
             <figcaption>${work.title}</figcaption>
             `;
-            document.querySelector(".gallery").appendChild(figure);
-        });
+            destination.appendChild(figure);
+});
+};
+
+
+export function showAllWorks() {
+//fonction permettant de tout afficher
+        fetchWorks().then(works => {
+        document.querySelector(".gallery").innerHTML = '';
+        displayWorks(works, document.querySelector(".gallery"))
         
     });
+}
+
+export function activeStyleButton(element, defaultClassName, newClassName) {
+  document.querySelectorAll('.' + defaultClassName).forEach(btn => btn.classList.remove(newClassName));
+  element.classList.add(newClassName);
 }
